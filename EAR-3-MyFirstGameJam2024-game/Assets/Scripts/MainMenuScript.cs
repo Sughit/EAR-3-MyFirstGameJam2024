1ab30de6ff5f9c182869adfaacd10ed587ca91d0
emanuel5867 <130173546+emanuel5867@users.noreply.github.com>
2024-07-27 15:49:15 +0300

    avem meniu si configurat audio

---
+++ EAR-3-MyFirstGameJam2024-game/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public GameObject main, settings, butonSettings, butonPlay, butonQuit;
+    void Awake()
+    {
+        butonSettings.SetActive(false);
+        butonPlay.SetActive(false);
+        butonQuit.SetActive(false);
+
+        StartCoroutine(Butoane());
+    }
+    public void PlayGame()
+    {
+        StartCoroutine(Butoane2());
+    }
+    public void Settings()
+    {
+        settings.SetActive(true);
+        main.SetActive(false);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    public void BackSettings()
+    {
+        settings.SetActive(false);
+        main.SetActive(true);
+    }
+    IEnumerator Butoane()
+    {
+        yield return new WaitForSeconds(0.08f);
+        butonQuit.SetActive(true);
+        yield return new WaitForSeconds(0.08f);
+        butonSettings.SetActive(true);
+        yield return new WaitForSeconds(0.08f);
+        butonPlay.SetActive(true);
+    }
+    IEnumerator Butoane2()
+    {
+        butonQuit.SetActive(false);
+        yield return new WaitForSeconds(0.08f);
+        butonSettings.SetActive(false);
+        yield return new WaitForSeconds(0.3f);
+        SceneManager.LoadScene("MainScene");
+    }
+}
