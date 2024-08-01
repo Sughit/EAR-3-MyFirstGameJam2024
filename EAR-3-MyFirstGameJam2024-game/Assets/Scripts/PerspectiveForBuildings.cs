6675a3abeef0b1f6d860dcffdd6e6ad670ded5b4
Sughit <andreisorici2007@gmail.com>
2024-08-01 09:24:35 +0300

    perspectiva pentru cladiri

---
+++ EAR-3-MyFirstGameJam2024-game/Assets/Scripts/PerspectiveForBuildings.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PerspectiveForBuildings : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.TryGetComponent<SpriteRenderer>(out SpriteRenderer renderer))
+        {
+            renderer.sortingOrder = 6;
+        }
+        else
+        {
+            SpriteRenderer ren = other.gameObject.GetComponentInChildren<SpriteRenderer>();
+            if(ren != null) ren.sortingOrder = 6;
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if(other.gameObject.TryGetComponent<SpriteRenderer>(out SpriteRenderer renderer))
+        {
+            renderer.sortingOrder = 6;
+        }
+        else
+        {
+            SpriteRenderer ren = other.gameObject.GetComponentInChildren<SpriteRenderer>();
+            if(ren != null) ren.sortingOrder = 6;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.gameObject.TryGetComponent<SpriteRenderer>(out SpriteRenderer renderer))
+        {
+            renderer.sortingOrder = 10;
+        }
+        else
+        {
+            SpriteRenderer ren = other.gameObject.GetComponentInChildren<SpriteRenderer>();
+            if(ren != null) ren.sortingOrder = 10;
+        }
+    }
+}
