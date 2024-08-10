8a28481c0e45798ed488f3c932eb3e22554dceab
Sughit <andreisorici2007@gmail.com>
2024-08-10 10:57:58 +0300

    enemy drops

--- EAR-3-MyFirstGameJam2024-game/Assets/Scripts/Enemy/EnemyHealth.cs
+++ EAR-3-MyFirstGameJam2024-game/Assets/Scripts/Enemy/EnemyHealth.cs
@@ -6,6 +6,9 @@ public class EnemyHealth : MonoBehaviour
 {
     float maxHealth = 100f;
     [SerializeField] private float health;
+    [SerializeField] private int numDrops;
+    [SerializeField] private GameObject[] drops;
+    [SerializeField] private float range;
     void Start()
     {
         health = maxHealth;
@@ -14,6 +17,18 @@ public class EnemyHealth : MonoBehaviour
     {
         health -= damage;
         if(health <= 0)
+        {
+            for(int i = 0; i < drops.Length; i++)
+            {
+                Instantiate(drops[Random.Range(0, drops.Length)], new Vector2(transform.position.x, transform.position.y) + Random.insideUnitCircle * range, Quaternion.identity);
+            }
             Destroy(gameObject);
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, range);
     }
 }
